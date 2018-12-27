2a3df0b0f4c892262e08630bc25215c8c41c2080
Midysen <miaodexing@126.com>
2018-12-27 16:59:00 +0800

    Add files via upload
    1、根据app下载地址，使用selenium自动打开浏览器，自动点击登录按钮
    2、点击登录按钮后进入登录界面，首先自动输入用户名，自动点击下一步按钮，进入输入密码页面
    3、输入密码页面，自动输入密码，自动点击下一步，进入app下载页面
    4、根据页面iframe定位安装按钮，实现自动点击下载    参照https://www.cnblogs.com/zhongyehai/p/9170366.html
    5、根据页面iframe定位ok按钮，实现自动点击ok按钮，完成安装

--- login.py
+++ login.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import time
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf8')
+# action行动 chains链
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class WebDriverTests(unittest.TestCase):
@@ -22,63 +21,67 @@ class WebDriverTests(unittest.TestCase):
 
 
 
-    def test_login(self):
-        #self.driver.get('https://play.google.com/store/apps/details?id=com.app.iod&rdid=com.app.iod&feature=md&offerId')
-        self.driver.get('https://play.google.com/store/apps/details?id=net.flixster.android&rdid=net.flixster.android&feature=md&offerId')
+    def test_01_login(self):
+        self.driver.get('https://play.google.com/store/apps/details?id=com.adobe.reader&rdid=com.adobe.reader&feature=md&offerId')
         self.driver.find_element_by_xpath('//div[@class="XfpsVe J9fJmf"]/div[@style="dIodBf"]/button[@class="LkLjZd ScJHi  nMZKrb mgVrBf xjAeve  "]').click()
         self.driver.implicitly_wait(30)
 
 
-        ## 找到用户名的输入框输入用户名并点击下一步输入密码
+        ## 找到用户名的输入框
         self.account_field = self.driver.find_elements_by_id('Email')
-
+        #根据用户名输入框来区分两种情况
         if not self.account_field:
-            print('1111111')
+            print('if start')
             ## 找到用户名的输入框输入用户名并点击下一步输入密码
             self.account_field = self.driver.find_element_by_id('identifierId')
             print(self.account_field)
             self.account_field.clear()
-            self.driver.implicitly_wait(30)
+            self.driver.implicitly_wait(10)
             self.account_field.send_keys('miaodexing@126.com')
             self.driver.find_element_by_id('identifierNext').click()
-            self.driver.implicitly_wait(30)
+            self.driver.implicitly_wait(10)
 
             time.sleep(3)
-            print('>>> 11111111')
             #找到密码框并输入密码，点击下一步登录
             self.password_field = self.driver.find_element_by_name('password')
             self.password_field.clear()
-            self.driver.implicitly_wait(30)
+            self.driver.implicitly_wait(10)
             self.password_field.send_keys('Nx%bmd_881225')
             self.driver.find_element_by_id('passwordNext').click()
-            self.driver.implicitly_wait(30)
-
-            self.driver.find_element_by_xpath('//button[@id="purchase-ok-button"]').click()
-
         else:
-            print('22222222')
+            print('else start')
             ## 找到用户名的输入框输入用户名并点击下一步输入密码
             self.account_field = self.driver.find_element_by_id('Email')
             print(self.account_field)
             self.account_field.clear()
-            self.driver.implicitly_wait(30)
+            self.driver.implicitly_wait(10)
             self.account_field.send_keys('miaodexing@126.com')
             self.driver.find_element_by_id('next').click()
-            self.driver.implicitly_wait(30)
+            self.driver.implicitly_wait(10)
             time.sleep(3)
-            print('>>> 22222222')
             #找到密码框并输入密码，点击下一步登录
             self.password_field = self.driver.find_element_by_name('Passwd')
             self.password_field.clear()
-            self.driver.implicitly_wait(30)
+            self.driver.implicitly_wait(10)
             self.password_field.send_keys('Nx%bmd_881225')
             self.driver.find_element_by_id('signIn').click()
-            self.driver.implicitly_wait(30)
+        print('-- login sucessfully -- ')
+        print()
 
-            self.driver.find_element_by_xpath('//button[@id="purchase-ok-button"]').click()
+    def test_02_install(self):
+        iframe = self.driver.find_elements_by_tag_name("iframe")[-1]
+        print(iframe)
+        self.driver.switch_to_frame(iframe)
+        time.sleep(4)
+        self.driver.implicitly_wait(10)
+        self.driver.find_element_by_xpath("//button[@id='purchase-ok-button']").click()
+        self.driver.implicitly_wait(10)
+        time.sleep(4)
+        self.driver.find_element_by_xpath("//button[@id='close-dialog-button']").click()
+        time.sleep(3)
+        self.driver.implicitly_wait(10)
 
-
-        print('-- test 02 finished -- ')
+        print('install ok!')
         print()
 
 
@@ -89,7 +92,7 @@ class WebDriverTests(unittest.TestCase):
         # close the browser window
         cls.driver.quit()
         pass
-        print('-- tear down finished -- ')
+        print('-- quit -- ')
         print()
 if __name__ == '__main__':
     unittest.main(verbosity=2)
