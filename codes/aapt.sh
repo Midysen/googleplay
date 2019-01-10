c2d36ffa6cb02d336c61c7d4fb66df1a8bbc152e
Midysen <miaodexing@126.com>
2019-01-10 17:22:03 +0800

    Add files via upload
    更改了以包名命名apk文件名

--- codes/aapt.sh
+++ codes/aapt.sh
@@ -2,8 +2,10 @@
 
 for apk in `ls /media/openthos/5FA55FB47D35DC39/apk/*/base.apk`
 do
-    aapt  dump badging $apk | grep versionName | cut -d "'" -f 6
-    aapt  dump badging $apk | grep versionName | cut -d "'" -f 2
-    aapt  dump badging $apk | grep versionName | cut -d "'" -f 4
-    ls -l $apk | cut -d ' ' -f 5
+    a=`aapt dump badging $apk | grep versionName | cut -d "'" -f 2`.apk
+    cp $apk ./download/$a
+    aapt  dump badging ./download/$a | grep versionName | cut -d "'" -f 6
+    aapt  dump badging ./download/$a | grep versionName | cut -d "'" -f 2
+    aapt  dump badging ./download/$a | grep versionName | cut -d "'" -f 4
+    ls -l ./download/$a | cut -d ' ' -f 5
 done
