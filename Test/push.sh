d0d357708777e484de64b6f0e2ac76178160a1fc
Midysen <miaodexing@126.com>
2019-02-26 13:45:32 +0800

    Update push.sh

--- Test/push.sh
+++ Test/push.sh
@@ -6,4 +6,5 @@ scp game lh@192.168.0.180:/var/www/html/openthos/appstores/data
 echo "push all apk"
 scp -r download lh@192.168.0.180:/var/www/html/openthos/appstores
 echo "push all icon"
+cp image icon -a
 scp -r icon lh@192.168.0.180:/var/www/html/openthos/appstores
